d8e90e574e583b28a21e10dd67e52a8f179fdd56
Hung Nguyen <sinhhung.ng@gmail.com>
2026-01-27 23:59:20 +0700

    Integrate validation schema

--- services-app-flight/bf-app/src/main/java/com/hungnguyen/flight/app/country/CountryRequestDto.java
+++ services-app-flight/bf-app/src/main/java/com/hungnguyen/flight/app/country/CountryRequestDto.java
@@ -1,10 +1,14 @@
 package com.hungnguyen.flight.app.country;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Country Request Object")
 public class CountryRequestDto {
 
+    @NotNull
+    @NotEmpty
     @Schema(description = "Name of the country", example = "United States")
     private String countryName;
 
