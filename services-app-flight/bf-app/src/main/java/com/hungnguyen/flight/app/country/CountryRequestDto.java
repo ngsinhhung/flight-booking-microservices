ad62a53827190ffed11f4d671fb2f2ac137d1aef
Hung Nguyen <104895357+ngsinhhung@users.noreply.github.com>
2026-01-28 00:05:43 +0700

    Merge pull request #9 from ngsinhhung/services-app-flight/master: Sync services-app-flight/master to master 28.01.2026
    * Merge pull request #6 from ngsinhhung/services-app-flight/fix-mybatis
    
    *fix mybatis
    
    * Merge pull request #7 from ngsinhhung/services-app-flight/integrate-swagger-api-docs
    
    * Integrate swagger api docs
    
    * Integrate swagger api docs
    
    * Merge pull request #8 from ngsinhhung/services-app-flight/integrate-validation-schema
    
    * Integrate validation schema

--- services-app-flight/bf-app/src/main/java/com/hungnguyen/flight/app/country/CountryRequestDto.java
+++ services-app-flight/bf-app/src/main/java/com/hungnguyen/flight/app/country/CountryRequestDto.java
@@ -1,7 +1,15 @@
 package com.hungnguyen.flight.app.country;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Schema(description = "Country Request Object")
 public class CountryRequestDto {
 
+    @NotNull
+    @NotEmpty
+    @Schema(description = "Name of the country", example = "United States")
     private String countryName;
 
     public String getCountryName() {
--- services-app-flight/bf-app/src/main/java/com/hungnguyen/flight/app/country/CountryResponseDto.java
+++ services-app-flight/bf-app/src/main/java/com/hungnguyen/flight/app/country/CountryResponseDto.java
@@ -1,8 +1,14 @@
 package com.hungnguyen.flight.app.country;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Country Response Object")
 public class CountryResponseDto {
 
+    @Schema(description = "Unique identifier of the country", example = "1")
     private Integer countryId;
+
+    @Schema(description = "Name of the country", example = "United States")
     private String countryName;
 
     public Integer getCountryId() {
