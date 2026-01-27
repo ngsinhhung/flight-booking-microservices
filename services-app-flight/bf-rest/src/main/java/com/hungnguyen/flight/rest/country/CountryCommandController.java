b27519eb9f677624ec8b2e731e78bd8eff919f99
Hung Nguyen <104895357+ngsinhhung@users.noreply.github.com>
2026-01-27 23:53:18 +0700

    Merge pull request #7 from ngsinhhung/services-app-flight/integrate-swagger-api-docs
    * Integrate swagger api docs
    
    * Integrate swagger api docs

--- services-app-flight/bf-rest/src/main/java/com/hungnguyen/flight/rest/country/CountryCommandController.java
+++ services-app-flight/bf-rest/src/main/java/com/hungnguyen/flight/rest/country/CountryCommandController.java
@@ -2,15 +2,18 @@ package com.hungnguyen.flight.rest.country;
 
 import com.hungnguyen.flight.app.country.CountryCommandApplicationService;
 import com.hungnguyen.flight.app.country.CountryRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/country")
+@Tag(name = "Country", description = "APIs for managing country data")
 public class CountryCommandController {
 
-    private CountryCommandApplicationService countryCommandApplicationService;
+    private final CountryCommandApplicationService countryCommandApplicationService;
 
     public CountryCommandController(CountryCommandApplicationService countryCommandApplicationService) {
         this.countryCommandApplicationService = countryCommandApplicationService;
@@ -19,6 +22,7 @@ public class CountryCommandController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add New Country", description = "API to add a new country")
     public ResponseEntity<Void> addNewCountry(@RequestBody CountryRequestDto dto) {
         countryCommandApplicationService.addNewCountry(dto);
         return new ResponseEntity<>(HttpStatus.CREATED);
--- services-app-flight/bf-rest/src/main/java/com/hungnguyen/flight/rest/country/CountryQueryController.java
+++ services-app-flight/bf-rest/src/main/java/com/hungnguyen/flight/rest/country/CountryQueryController.java
@@ -2,6 +2,8 @@ package com.hungnguyen.flight.rest.country;
 
 import com.hungnguyen.flight.app.country.CountryQueryApplicationService;
 import com.hungnguyen.flight.app.country.CountryResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/country")
+@Tag(name = "Country", description = "APIs for retrieving country data")
 public class CountryQueryController {
 
     private final CountryQueryApplicationService countryQueryApplicationService;
@@ -23,6 +26,7 @@ public class CountryQueryController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get Countries", description = "API to retrieve the list of countries")
     public ResponseEntity<List<CountryResponseDto>> getCountries() {
         return new ResponseEntity<>(countryQueryApplicationService.getCountries(), HttpStatus.OK);
     }
